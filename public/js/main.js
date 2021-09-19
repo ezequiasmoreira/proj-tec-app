0ce07178f66c15d35faa481cbd7eb778c665fd4a
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2021-09-18 21:47:19 -0300

    Refatorado diretório

--- public/js/main.js
+++ public/js/main.js
@@ -2,17 +2,16 @@ angular.module('projetoTecnico', ['projetoTecnicoDiretivas','ngRoute','ui.router
 .config(function($routeProvider){  
     
     $routeProvider.when('/inicio',{
-        templateUrl:'pages/visao-geral.html',
+        templateUrl:'visao-geral.html',
         controller: 'projetoTecnicoController'
     });
 
     $routeProvider.when('/cliente-cadastro',{
-        templateUrl:'pages/cliente/cadastro.html',
+        templateUrl:'cliente/cliente.html',
         controller: 'clienteController',
     });
     
     $routeProvider.when('/fornecedor-cadastro',{
-        url: 'fornecedor',
         templateUrl:'fornecedor/fornecedor.html',
         controller: 'fornecedorController'
     });
